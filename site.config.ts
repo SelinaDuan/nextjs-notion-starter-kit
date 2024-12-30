f5683f5fe17a4c922cf7a468a3b2d0db7a59654f
SelinaDuan <100548893+SelinaDuan@users.noreply.github.com>
2024-12-30 17:39:18 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,16 +2,16 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '613ee3afda7b406c9cac7ba2965502cc',
+  rootNotionPageId: '16c9fd4f6177807aa7ddf362bf8b9206',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Chaodi Tang Blog World',
-  domain: 'chaodit.com',
-  author: 'Chaodi Tang',
+  name: 'Yixi Duan Blog World',
+  domain: 'innobrain.online',
+  author: 'Yixi Duan',
 
   // open graph metadata (optional)
   description: 'My Blogs.',
